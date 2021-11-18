24a1237ae3fff4dfdc1d67a2643a4375137d101c
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-18 09:25:37 +0200

    toggleUsersHandler.bind

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,7 +9,41 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Jane' },
 ];
 
-const Users = () => {
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    // state always has to be one object !!! cannot be more than one
+    this.state = {
+      showUsers: true,
+      someMoreVar: 5,
+    };
+  }
+  // method
+  toggleUsersHandler() {
+    // this merges with this.state and does not ovewrite
+    this.setState({ showUsers: false });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+const Users1 = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {

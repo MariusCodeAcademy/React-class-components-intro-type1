20a0507977b7cc315f3efb280c7ae08b533fa8f1
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-18 11:28:00 +0200

    class context

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ const DUMMY_USERS = [
   { id: 'u1', name: 'James' },
   { id: 'u2', name: 'Mike' },
   { id: 'u3', name: 'Jane' },
+  { id: 'u4', name: 'Michel' },
 ];
 
 function App() {
-  const usersContext = DUMMY_USERS;
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
   return (
     <UsersContext.Provider value={usersContext}>
       <UserFinder />
--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,14 +1,11 @@
 import css from './UserFinder.module.css';
 import { useState, useEffect, Component } from 'react';
 import Users from './Users';
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'James' },
-  { id: 'u2', name: 'Mike' },
-  { id: 'u3', name: 'Jane' },
-];
+import UsersContext from '../store/UsersContext';
 
 class UserFinder extends Component {
+  // 1 to use users context
+  static contextType = UsersContext;
   constructor(props) {
     super(props);
     this.state = {
@@ -20,14 +17,15 @@ class UserFinder extends Component {
 
   componentDidMount() {
     console.log('componentDidMount');
-    this.setState({ filteredUsers: DUMMY_USERS });
+    // 2 use context values
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate');
     // if searchTerm changed i will update
     if (prevState.searchTerm !== this.state.searchTerm) {
-      const filteredArr = DUMMY_USERS.filter((userObj) =>
+      const filteredArr = this.context.users.filter((userObj) =>
         userObj.name.includes(this.state.searchTerm)
       );
       this.setState({ filteredUsers: filteredArr });
@@ -39,6 +37,7 @@ class UserFinder extends Component {
   searchChangeHandler = (e) => {
     this.setState({ searchTerm: e.target.value });
   };
+
   render() {
     return (
       <>
@@ -59,41 +58,41 @@ class UserFinder extends Component {
 }
 // CLASS END
 
-function UserFinder1() {
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+// function UserFinder1() {
+//   const [filteredUsers, setFilteredUsers] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState('');
 
-  const searchChangeHandler = (e) => {
-    setSearchTerm(e.target.value);
-  };
-  // componentDidMount = useEffect(() => {}, [])
-  useEffect(() => {
-    setFilteredUsers(DUMMY_USERS);
-    // componentWillUnmount()
-    return () => console.log('clenup');
-  }, []);
+//   const searchChangeHandler = (e) => {
+//     setSearchTerm(e.target.value);
+//   };
+//   // componentDidMount = useEffect(() => {}, [])
+//   useEffect(() => {
+//     setFilteredUsers(DUMMY_USERS);
+//     // componentWillUnmount()
+//     return () => console.log('clenup');
+//   }, []);
 
-  // componentDidUpdate - class based
-  useEffect(() => {
-    console.log('searchTerm updated', searchTerm);
-    const filteredArr = DUMMY_USERS.filter((userObj) =>
-      userObj.name.includes(searchTerm)
-    );
-    setFilteredUsers(filteredArr);
-  }, [searchTerm]);
+//   // componentDidUpdate - class based
+//   useEffect(() => {
+//     console.log('searchTerm updated', searchTerm);
+//     const filteredArr = DUMMY_USERS.filter((userObj) =>
+//       userObj.name.includes(searchTerm)
+//     );
+//     setFilteredUsers(filteredArr);
+//   }, [searchTerm]);
 
-  return (
-    <>
-      <div className={css.finder}>
-        <input
-          type='search'
-          value={searchTerm}
-          onChange={searchChangeHandler}
-        />
-      </div>
-      <Users users={filteredUsers} />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div className={css.finder}>
+//         <input
+//           type='search'
+//           value={searchTerm}
+//           onChange={searchChangeHandler}
+//         />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </>
+//   );
+// }
 
 export default UserFinder;

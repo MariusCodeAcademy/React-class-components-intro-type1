c6dea5b60807f5596d49ecd35f32f6c2ec8fc626
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-18 11:19:12 +0200

    createUserCtx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import UserFinder from './components/UserFinder';
+import UsersContext from './store/UsersContext';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'James' },
+  { id: 'u2', name: 'Mike' },
+  { id: 'u3', name: 'Jane' },
+];
 
 function App() {
+  const usersContext = DUMMY_USERS;
   return (
-    <div>
+    <UsersContext.Provider value={usersContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
